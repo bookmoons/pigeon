0578388b4b9e414fe5d526cbed11a436614f1cbf
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-09-06 00:51:02 -0400

    fuzz: Test JSON result against encoding/json

--- examples/json/json_fuzz.go
+++ examples/json/json_fuzz.go
@@ -2,11 +2,26 @@
 
 package json
 
+import (
+	"encoding/json"
+	"reflect"
+)
+
 // Fuzz tests the JSON parser.
 func Fuzz(fuzz []byte) int {
-	_, err := Parse("fuzz", fuzz)
+	var expected interface{}
+	expectedErr := json.Unmarshal(fuzz, expected)
+	result, err := Parse("fuzz", fuzz)
 	if err != nil {
+		if expectedErr == nil {
+			panic("incorrect failure")
+		}
 		return 0
+	} else if expectedErr != nil {
+		panic("incorrect success")
+	}
+	if !reflect.DeepEqual(result, expected) {
+		panic("unexpected result")
 	}
 	return 1
 }
